b0729f6fe6b084b113a391776cd96fd2c42ad095
Abdallah Alaraby <abdallah.alaraby@nabdacare.com>
2014-01-20 23:10:42 +0200

    Commiting MinMax.java

---
+++ src/com/jjoe64/graphview/MinMax.java
@@ -0,0 +1,12 @@
+package com.jjoe64.graphview;
+
+public class MinMax {
+	
+	public MinMax(double min, double max) {
+		this.min = min;
+		this.max = max;
+	}
+
+	public double min;
+	public double max;
+}
